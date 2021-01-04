8ae68d1afaa4d1a5991c99673d5b1df7fcc869f6
qcyblm <qcyblm@163.com>
2021-01-04 21:33:18 +0800

    Update documents.

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@ set -e
 yarn
 git add .
 git commit -S -m 'Update documents.'
+git push
 yarn build
 cd src/.vuepress/dist
 git init -b master
 git add -A
 git commit -S -m "deploy: $(TZ="Asia/Shanghai" date +"%Y-%m-%d %T %Z")"
-git push -f $giteeUrl master:gh-pages
+git push -f git@gitee.com:qcyblm/qcyblm.git master:gh-pages
 cd -
