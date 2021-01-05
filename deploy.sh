e2ca0e0abd9d0c1eb46797f3cf591659ce94cbb1
qcyblm <qcyblm@163.com>
2021-01-05 15:50:31 +0800

    Update documents.

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,9 @@ git push
 yarn build
 cd src/.vuepress/dist
 git init -b master
+git config user.name 'qcyblm'
+git config user.email 'qcyblm@163.com'
 git add -A
-git commit -S -m "deploy: $(TZ="Asia/Shanghai" date +"%Y-%m-%d %T %Z")"
+git commit -m "deploy: $(TZ="Asia/Shanghai" date +"%Y-%m-%d %T %Z")"
 git push -f git@gitee.com:qcyblm/qcyblm.git master:gh-pages
 cd -
