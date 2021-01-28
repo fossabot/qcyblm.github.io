a48225804423e1e7454fbba654c63f3c2174f94c
qcyblm <qcyblm@163.com>
2021-01-28 10:00:40 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git config user.name 'qcyblm'
 git config user.email '8064994+qcyblm@user.noreply.gitee.com'
 git add -A
-git commit -m "deploy: $(date +"%Y年%m月%d日 %T %Z")"
+git commit -m "deploy: $(date +"%Y年%m月%d日 %T %:z")"
 git push -f git@gitee.com:qcyblm/qcyblm.git master:gh-pages
 cd -
