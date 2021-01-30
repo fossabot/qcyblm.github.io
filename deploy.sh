5ab7c60854b5f660e33f5b9be2b4a3d3199a2817
qcyblm <qcyblm@163.com>
2021-01-30 12:06:03 +0800

    Replace the replication directory.

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cd gh-pages
 git config user.name 'qcyblm'
 git config user.email '8064994+qcyblm@user.noreply.gitee.com'
 rm -rf *
-cp -a ../dist gh-pages
+cp -a ../dist .
 git add -A
 git commit -m "deploy: $(date +"%Y年%m月%d日 %T %:z")"
 git push origin gh-pages
