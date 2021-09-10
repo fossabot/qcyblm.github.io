cfd55371eae6704c686b733c6be33129fb25a954
qcyblm <qcyblm@139.com>
2021-09-10 15:28:54 +0800

    Update config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -72,26 +72,6 @@ export default defineUserConfig<DefaultThemeOptions>({
 		lastUpdatedText: '最后更新时间',
 	},
 	plugins: [
-		[
-			'@vuepress/plugin-search',
-			{
-				placeholder: '搜索',
-			},
-		],
-		[
-			'@vuepress/last-updated',
-			{
-				dateOptions: {
-					hour12: false,
-					timeZone: 'Asia/Shanghai',
-					year: 'numeric',
-					month: '2-digit',
-					day: '2-digit',
-					hour: '2-digit',
-					minute: '2-digit'
-				}
-			}
-		],
 		['@vuepress/plugin-pwa'],
 		[
 			'@vuepress/plugin-pwa-popup',
