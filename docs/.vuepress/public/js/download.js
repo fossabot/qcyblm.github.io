bef51cafb15b392f60fa8d4ba54b0eb9d773293d
qcyblm <qcyblm@139.com>
2021-09-10 14:58:31 +0800

    Update download.js

--- docs/.vuepress/public/js/download.js
+++ docs/.vuepress/public/js/download.js
@@ -1,12 +1,36 @@
-function github_download(download_url, option) {
+function github_download_error(error_link) {
+	console.log("无此文件，正在跳转" + error_link);
+	window.open(error_link, '_blank').focus();
+	//window.location.replace(error_link);
+}
+function github_download(repositories, option) {
 	var xhttp = new XMLHttpRequest();
+	var error_link = "https://github.com/" + repositories + "/releases";
 	xhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
-			var url = JSON.parse(xhttp.responseText).assets[option].browser_download_url;
-			window.location.replace(url);
-			window.location.href = url;
+			try {
+				var content = JSON.parse(xhttp.responseText);
+				var url = content.assets[option].browser_download_url;
+				var name = content.assets[option].name;
+				console.log("正在下载 " + name + " 请稍等……");
+				window.location.replace(url);
+			} catch (error) {
+				github_download_error(error_link);
+			}
 		}
 	};
-	xhttp.open("GET", "https\:\/\/api.github.com\/repos\/" + download_url + "\/releases\/latest", true);
+	xhttp.onload = function () {
+		if (this.readyState != 4 && this.status != 200) {
+			github_download_error(error_link);
+		}
+	};
+	xhttp.onerror = function () {
+		github_download_error(error_link);
+	};
+	xhttp.ontimeout = function (e) {
+		github_download_error(error_link);
+	};
+	xhttp.open("GET", "https://api.github.com/repos/" + repositories + "/releases/latest", true);
+	xhttp.timeout = 2000;
 	xhttp.send();
 }
